b7e11828a9a6156869b753a0d5f9c43798457d41
nicki1377 <76948246+nicki1377@users.noreply.github.com>
2023-02-15 16:52:46 -0600

    uhhhhhh boulder interaction kinda

--- Assets/Scripts/SlidingBox.cs
+++ Assets/Scripts/SlidingBox.cs
@@ -3,19 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlidingBox : MonoBehaviour
+public class SlidingBox : MonoBehaviour, IInteractable
 {
+
+    /// <summary>
+    /// Indicates whether the boulder is currently being pulled or not
+    /// </summary>
+    public bool isPull;
+
+    /// <summary>
+    /// Rigidbody component of this boulder
+    /// </summary>
+    Rigidbody2D _rb;
+
+    /// <summary>
+    /// Player that interacts with the boulder
+    /// </summary>
+    [SerializeField] private GameObject playerObject;
+
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        isPull = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPull)
+        {
+            //_rb.velocity = playerObject.transform.velocity;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -24,5 +44,11 @@ public class SlidingBox : MonoBehaviour
         {
             collision.gameObject.GetComponent<NPC>().UpdateHealth(-50);
         }
+    }
+
+    public bool Interact(Interactor interactor)
+    {
+        isPull = !isPull;
+        return true;
     }
 }
