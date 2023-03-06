6cbf37d2a3ca0d1ce10848e995dd7ef83d0a6046
mercedes sandu <mxrcedes@gmail.com>
2023-03-06 02:52:49 -0600

    mirrors wip

--- Assets/Scripts/Game Objects/Mirror.cs
+++ Assets/Scripts/Game Objects/Mirror.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
@@ -8,32 +9,32 @@ public class Mirror : MonoBehaviour, IInteractable
     /// The endpoint of the laser.
     /// </summary>
     [SerializeField] private Transform laserEndpoint;
-    
+
     /// <summary>
     /// Degrees to rotate the window by upon interaction.
     /// </summary>
     [SerializeField] private int degrees = 25;
 
     /// <summary>
-    /// The box collider for the laser.
+    /// The distance the laser travels.
     /// </summary>
-    [SerializeField] private BoxCollider2D laserCollider;
+    [SerializeField] private float distance = 15;
 
     /// <summary>
-    /// (hard coded) the number of interactions required to kill the NPC.
+    /// The damage the laser does to the player.
     /// </summary>
-    [SerializeField] private int numInteractionsRequired;
+    [SerializeField] private int damageToPlayer = -10;
 
     /// <summary>
-    /// (hard coded) the NPC to be killed.
+    /// The damage the laser does to the NPC.
     /// </summary>
-    [SerializeField] private NPC npc;
+    [SerializeField] private int damageToNPC = -50;
 
     /// <summary>
-    /// (hard coded) the current number of interactions.
+    /// The amount of time the player is invulnerable after being hit by the laser.
     /// </summary>
-    private int _numInteractions = 0;
-    
+    [SerializeField] private float invulnerableTime = 1;
+
     /// <summary>
     /// True when mirror has been completely turned to the right. False when completely turned to the left.
     /// </summary>
@@ -55,6 +56,11 @@ public class Mirror : MonoBehaviour, IInteractable
     private Vector3 _endPos;
 
     /// <summary>
+    /// True if the player is invulnerable, false otherwise.
+    /// </summary>
+    private bool _invulnerable = false;
+
+    /// <summary>
     /// Initializes the laser.
     /// </summary>
     private void Awake()
@@ -65,7 +71,6 @@ public class Mirror : MonoBehaviour, IInteractable
         _endPos = laserEndpoint.localPosition;
         _laser.SetPosition(0, _startPos);
         _laser.SetPosition(1, _endPos);
-        // ModifyLaserCollider();
     }
 
     /// <summary>
@@ -75,8 +80,9 @@ public class Mirror : MonoBehaviour, IInteractable
     {
         _endPos = laserEndpoint.localPosition;
         _laser.SetPosition(1, _endPos);
+        DetectCollisions();
     }
-    
+
     /// <summary>
     /// Calls for the UI to be updated.
     /// </summary>
@@ -85,16 +91,15 @@ public class Mirror : MonoBehaviour, IInteractable
     public bool Interact(Interactor interactor)
     {
         Transform t = transform;
-        
+
         if (transform.rotation.y > Quaternion.Euler(new Vector3(0, 40, 0)).y)
         {
             _turned = true;
         }
 
-        if (transform.rotation.y < Quaternion.Euler(new Vector3 (0, 50, 0)).y & !_turned)
+        if (transform.rotation.y < Quaternion.Euler(new Vector3(0, 50, 0)).y & !_turned)
         {
             laserEndpoint.transform.RotateAround(t.position, t.forward, degrees);
-            // ModifyLaserCollider();
             transform.rotation *= Quaternion.Euler(Vector3.up * degrees);
         }
         else
@@ -102,66 +107,46 @@ public class Mirror : MonoBehaviour, IInteractable
             if (transform.rotation.y > Quaternion.Euler(new Vector3(0, -40, 0)).y)
             {
                 laserEndpoint.transform.RotateAround(t.position, t.forward, -degrees);
-                // ModifyLaserCollider();
                 transform.rotation *= Quaternion.Euler(Vector3.up * -degrees);
             }
             else _turned = false;
         }
 
-        _numInteractions++;
-        
-        if (_numInteractions == numInteractionsRequired) npc.UpdateHealth(-npc.GetCurrentHealth());
-
         return true;
     }
 
-    // /// <summary>
-    // /// Modifies the laser collider.
-    // /// </summary>
-    // private void ModifyLaserCollider()
-    // {
-    //     Vector3 startPos = transform.position;
-    //     Vector3 endPos = laserEndpoint.position;
-    //     
-    //     // set size of collider to length of line
-    //     float lineLength = Vector3.Distance(startPos, endPos);
-    //     laserCollider.size = new Vector3(lineLength, _laser.startWidth);
-    //     
-    //     // move collider to the middle of the line
-    //     Vector3 midPoint = (startPos + endPos) / 2;
-    //     laserCollider.transform.position = midPoint;
-    //     
-    //     // rotate the collider to match the rotation of the line
-    //     float angle = Mathf.Abs(startPos.y - endPos.y) / Mathf.Abs(startPos.x - endPos.x);
-    //     if ((startPos.y < endPos.y && startPos.x > endPos.x) ||
-    //         (endPos.y < startPos.y && endPos.x > startPos.x))
-    //     {
-    //         angle *= -1;
-    //     }
-    //     angle = Mathf.Rad2Deg * Mathf.Atan(angle);
-    //     laserCollider.transform.Rotate(0, 0, angle);
-    // }
+    /// <summary>
+    /// Detects collisions with the laser and applies damage to the player and NPCs.
+    /// </summary>
+    private void DetectCollisions()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, 
+            laserEndpoint.transform.position - transform.position, distance);
+        foreach (RaycastHit2D hit in hits)
+        {
+            Debug.DrawRay(transform.position, hit.collider.transform.position - transform.position, Color.green);
+            if (hit.collider.CompareTag("NPC"))
+            {
+                Debug.Log("hit npc " + hit.collider.name + " with mirror " + name);
+                hit.collider.GetComponent<NPC>().UpdateHealth(damageToNPC);
+            }
+            if (hit.collider.CompareTag("Player"))
+            {
+                if (_invulnerable) return;
+                PlayerControl.Instance.UpdateHealth(damageToPlayer);
+                StartCoroutine(Cooldown());
+            }
+        }
+    }
 
-    // /// <summary>
-    // /// Kills NPCs on impact (rip).
-    // /// </summary>
-    // /// <param name="col">The collider.</param>
-    // private void OnTriggerEnter2D(Collider2D col)
-    // {
-    //     if (!col.CompareTag("NPC")) return;
-    //     Debug.Log("collided with NPC");
-    //     NPC npc = col.GetComponent<NPC>();
-    //     npc.UpdateHealth(-npc.GetCurrentHealth());
-    // }
-    // /// <summary>
-    // /// Kills NPCs on impact (rip).
-    // /// </summary>
-    // /// <param name="col">The collision.</param>
-    // private void OnCollisionEnter2D(Collision2D col)
-    // {
-    //     if (!col.collider.CompareTag("NPC")) return;
-    //     Debug.Log("collided with NPC");
-    //     NPC npc = col.collider.GetComponent<NPC>();
-    //     npc.UpdateHealth(-npc.GetCurrentHealth());
-    // }
+    /// <summary>
+    /// Counts down the invulnerability time.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Cooldown()
+    {
+        _invulnerable = true;
+        yield return new WaitForSeconds(invulnerableTime);
+        _invulnerable = false;
+    }
 }
