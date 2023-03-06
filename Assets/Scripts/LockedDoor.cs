1d109c62e2b12e7b531f03f6d4821e9879250d7e
nicki1377 <76948246+nicki1377@users.noreply.github.com>
2023-03-06 14:57:20 -0600

    key and door functioning

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -10,21 +10,11 @@ public class LockedDoor : MonoBehaviour, IInteractable
 
     PlayerControl playerControl;
 
-    [SerializeField] GameObject tilemapObject;
-
-    Tilemap tilemap;
-
-    [SerializeField] private Vector3Int doorCoords;
-
-    [SerializeField] private int doorWidth;
-
-    [SerializeField] private int doorHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControl = playerObject.GetComponent<PlayerControl>();
-        tilemap = tilemapObject.GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -37,14 +27,6 @@ public class LockedDoor : MonoBehaviour, IInteractable
     {
         if (playerControl.hasKey)
         {
-            // something something tilemap destroy
-            for (int i = 0; i < doorWidth; i++)
-            {
-                for (int j = 0; j < doorHeight; j++)
-                {
-                    tilemap.SetTile(new Vector3Int(doorCoords.x + i, doorCoords.y + j, doorCoords.z), null);
-                }
-            }
             gameObject.SetActive(false);
         }
         else
